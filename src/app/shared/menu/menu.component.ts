bfd1b488e126d3e00b56df169ac25a776b524b4a
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-20 22:35:05 -0500

    menu componenet v1.0

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
+interface MenuItem {
+  ruta: string,
+  nombre: string,
+}
 @Component({
   selector: 'ez-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
+  @ViewChild('header') header!: ElementRef;
 
-  ngOnInit(): void {
+  constructor(private renderer2: Renderer2) {}
+
+  isDown: boolean = false;
+
+  headerDown() {
+    this.isDown = !this.isDown
   }
 
+  menuItems: MenuItem[] = [
+    {
+      ruta: '/mapas/full-screen',
+      nombre: 'Full Screen'
+    },
+    {
+      ruta: '/mapas/zoom-range',
+      nombre: 'Zoom Range'
+    },
+    {
+      ruta: '/mapas/marcadores',
+      nombre: 'Marcadores'
+    },
+    {
+      ruta: '/mapas/propiedades',
+      nombre: 'Propiedades'
+    }
+  ];
 }
