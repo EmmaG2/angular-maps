58299e1c81d28d226ddeac698e9fdf79e3530baa
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-20 20:19:17 -0500

    lazy load

--- src/app/mapas/mapas-routing.module.ts
+++ src/app/mapas/mapas-routing.module.ts
@@ -1,7 +1,37 @@
+import { MarcadoresComponent } from './pages/marcadores/marcadores.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'full-screen',
+        component: FullScreenComponent
+      },
+      {
+        path: 'zoom-range',
+        component: ZoomRangeComponent
+      },
+      {
+        path: 'marcadores',
+        component: MarcadoresComponent
+      },
+      {
+        path: 'propiedades',
+        component: PropiedadesComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'full-screen'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
