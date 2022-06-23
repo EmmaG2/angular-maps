a3ac6e64af8a8ac44246d0490abde951abf32d83
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-23 16:11:32 -0500

    global map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl  from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularMaps';
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = environment.token;
+  }
 }
