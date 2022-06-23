28344099998adcf1ca64778f98ccfa1d7e1db6c8
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-23 16:12:27 -0500

    zoom range ts

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'ez-zoom-range',
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.scss']
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('map') MapaRef!: ElementRef;
+  mapa!: mapboxgl.Map;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngAfterViewInit(): void {
+
+    console.log('afterviewinit', this.MapaRef);
+
+    this.mapa = new mapboxgl.Map({
+      container: this.MapaRef.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-104.60306427141977, 24.043922542308422  ],
+      zoom: 18
+    });
+  }
+
+  zoomOut() {
+    console.log('zoom out', this.MapaRef);
+    // this.mapa.zoomOut();
+  }
+
+  zoomIn() {
+    this.mapa.zoomIn();
   }
 
 }
