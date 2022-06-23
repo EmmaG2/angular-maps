50af365f700e814e992f9690aca5683e4e575bfd
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-23 16:09:06 -0500

    refactor: id="mapa" to #map

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'ez-full-screen',
   templateUrl: './full-screen.component.html',
   styleUrls: ['./full-screen.component.scss']
 })
-export class FullScreenComponent implements OnInit {
+export class FullScreenComponent implements AfterViewInit {
+
+  @ViewChild('map') MapRef!: ElementRef;
+  mapa!: mapboxgl.Map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.MapRef.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-104.60306427141977, 24.043922542308422  ],
+      zoom: 18
+    });
   }
 
 }
