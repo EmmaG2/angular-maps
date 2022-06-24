5673926b619a8889aa301c159f9be97b347b34c5
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-24 14:45:27 -0500

    updated: propiedades component

--- src/app/mapas/pages/propiedades/propiedades.component.ts
+++ src/app/mapas/pages/propiedades/propiedades.component.ts
@@ -1,17 +1,21 @@
 import { Title } from '@angular/platform-browser';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'ez-propiedades',
   templateUrl: './propiedades.component.html',
   styleUrls: ['./propiedades.component.scss']
 })
-export class PropiedadesComponent implements OnInit {
+export class PropiedadesComponent implements OnInit, OnDestroy {
 
   constructor(private title: Title) { }
 
   ngOnInit(): void {
     this.title.setTitle('Propiedades - Angular Maps')
+  }
+
+  ngOnDestroy(): void {
+    console.clear()
   }
 
 }
