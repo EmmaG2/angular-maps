f0f304d4b8869067ecd68cb6ff883eb89258b6d9
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-26 22:42:38 -0500

    ondestroy console clear

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import * as mapboxgl from 'mapbox-gl';
 
@@ -7,7 +7,7 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './full-screen.component.html',
   styleUrls: ['./full-screen.component.scss']
 })
-export class FullScreenComponent implements AfterViewInit, OnInit {
+export class FullScreenComponent implements AfterViewInit, OnInit, OnDestroy {
 
   @ViewChild('map') MapRef!: ElementRef;
   mapa!: mapboxgl.Map;
@@ -22,9 +22,13 @@ export class FullScreenComponent implements AfterViewInit, OnInit {
     this.mapa = new mapboxgl.Map({
       container: this.MapRef.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-104.60306427141977, 24.043922542308422  ],
+      center: [-104.60306427141977, 24.043922542308422],
       zoom: 18
     });
+  }
+
+  ngOnDestroy(): void {
+    console.clear()
   }
 
 }
