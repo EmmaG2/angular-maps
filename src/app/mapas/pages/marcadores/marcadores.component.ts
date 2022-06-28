a473680eaa60ae63ac25d4bc684d8bcd7ee48df5
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-28 11:37:11 -0500

    marcadores component ts updated

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
+
+interface MarkerColor {
+  color: string;
+  center?: [number, number],
+  marker?: mapboxgl.Marker;
+}
 
 @Component({
   selector: 'ez-marcadores',
@@ -10,13 +16,14 @@ import { environment } from 'src/environments/environment';
 })
 export class MarcadoresComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor(private title: Title) { }
-
   @ViewChild('map') MapaRef!: ElementRef;
+
+  constructor(private title: Title, private renderer2: Renderer2) { }
 
   public mapa!: mapboxgl.Map;
   public center: [number, number] = [-104.60306427141977, 24.043922542308422];
-
+  public markersArray: MarkerColor[] = [];
+  public showModal: boolean = false;
 
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
@@ -26,9 +33,13 @@ export class MarcadoresComponent implements OnInit, OnDestroy, AfterViewInit {
       zoom: 16
     });
 
-    const marker = new mapboxgl.Marker({ color: '#f22445' })
-      .setLngLat(this.center)
-      .addTo(this.mapa)
+    this.leerLocalStorage();
+
+    this.markersArray.forEach(m => {
+      m.marker?.on('dragend', () => {
+        this.guardarMarcadores()
+      })
+    })
   }
 
   ngOnInit(): void {
@@ -37,6 +48,92 @@ export class MarcadoresComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnDestroy(): void {
     console.clear()
+  }
+
+  public agregarMarcador(): void {
+
+    if (this.markersArray.length >= 8) {
+      this.showModal = true;
+      return;
+    }
+
+    const color = "#xxxxxx".replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+
+    const newMarker: mapboxgl.Marker = new mapboxgl.Marker({
+      color,
+      draggable: true
+    })
+      .setLngLat(this.center)
+      .addTo(this.mapa)
+
+    this.markersArray.push({
+      color,
+      marker: newMarker
+    });
+
+    this.guardarMarcadores();
+
+    newMarker.on('dragend', () => {
+      this.guardarMarcadores();
+    })
+  }
+
+  public goToMark(marker: mapboxgl.Marker): void {
+    this.mapa.flyTo({
+      center: marker.getLngLat(),
+      zoom: 18,
+      animate: true
+    });
+  }
+
+  public guardarMarcadores(): void {
+
+    const lngLatArray: MarkerColor[] = [];
+
+    this.markersArray.forEach(m => {
+      const color = m.color;
+      const { lng, lat } = m.marker!.getLngLat();
+
+      lngLatArray.push({
+        color,
+        center: [lng, lat]
+      })
+
+      localStorage.setItem('marcadores', JSON.stringify(lngLatArray))
+
+    })
+  }
+
+  public leerLocalStorage(): void {
+    if (!localStorage.getItem('marcadores')) return;
+
+    const lngLatArray: MarkerColor[] = JSON.parse(localStorage.getItem('marcadores')!);
+
+    lngLatArray.forEach(m => {
+      const marker = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true,
+      })
+        .setLngLat(m.center!)
+        .addTo(this.mapa)
+
+      this.markersArray.push({
+        marker,
+        color: m.color
+      })
+    })
+  }
+
+  public ocultarModal(): void {
+    this.showModal = false;
+
+  }
+
+  public borrarMarcador(e: MouseEvent, i: number): void {
+    e.preventDefault();
+
+    this.markersArray[i].marker?.remove();
+    this.markersArray.splice(i, 1)
   }
 
 }
