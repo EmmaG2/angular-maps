cd1288dc3b76496e593c8362d0d4715991c62b75
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-29 21:56:14 -0500

    mini map component finished

--- src/app/mapas/core/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/core/mini-mapa/mini-mapa.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'ez-mini-mapa',
-  templateUrl: './mini-mapa.component.html',
-  styleUrls: ['./mini-mapa.component.scss']
+  template: '<div #mapa></div>',
+  styles: [`
+    div {
+      width: 250px;
+      height: 250px;
+      margin: 0;
+    }
+  `]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
 
+  @Input() lngLat: [number, number] = [0, 0];
+  @ViewChild('mapa') mapaRef!: ElementRef;
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
+    const mapa = new mapboxgl.Map({
+      container: this.mapaRef.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      accessToken: environment.token,
+      center: this.lngLat,
+      zoom: 17,
+      interactive: false
+    })
+
+    new mapboxgl.Marker({
+      color: '#be1558'
+    })
+      .setLngLat(this.lngLat)
+      .addTo(mapa)
+
   }
 
 }
