3d96c9c435e2836ae30b4c587eb55e281cb34cc1
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-06-29 21:58:21 -0500

    propiedades component finished

--- src/app/mapas/pages/propiedades/propiedades.component.ts
+++ src/app/mapas/pages/propiedades/propiedades.component.ts
@@ -1,14 +1,21 @@
-import { Title } from '@angular/platform-browser';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
+
+interface Propiedad {
+  titulo: string;
+  descripcion: string;
+  lngLat: [number, number];
+}
 
 @Component({
   selector: 'ez-propiedades',
   templateUrl: './propiedades.component.html',
-  styleUrls: ['./propiedades.component.scss']
+  styleUrls: ['./propiedades.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PropiedadesComponent implements OnInit, OnDestroy {
 
-  constructor(private title: Title) { }
+  constructor(private title: Title, private meta: Meta) { }
 
   ngOnInit(): void {
     this.title.setTitle('Propiedades - Angular Maps')
@@ -17,5 +24,28 @@ export class PropiedadesComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     console.clear()
   }
+
+  propiedades: Propiedad[] = [
+    {
+      titulo: 'Casa residencial, Canadá',
+      descripcion: 'Bella propiedad en Katana, Canadá',
+      lngLat: [-75.92722289474008, 45.280015511264466]
+    },
+    {
+      titulo: 'Casa de playa, México',
+      descripcion: 'Hermosa casa de playa en Acapulco, México',
+      lngLat: [-99.91287720907991, 16.828940930185748]
+    },
+    {
+      titulo: 'Apartamento, Argentina',
+      descripcion: 'Lujoso apartamento en el corazón de Buenos Aires, Argentina',
+      lngLat: [-58.430166677283445, -34.57150108832866]
+    },
+    {
+      titulo: 'Local comercial, España',
+      descripcion: 'Local comercial disponible en Madrid, España, cerca de El Jardín Secreto.',
+      lngLat: [-3.7112735618380177, 40.42567285425766]
+    },
+  ]
 
 }
